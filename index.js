2e114a6881d966d84bae243f3a4cf8046af201c8
kunal. <kunalsalve2411@gmail.com>
2024-02-02 12:00:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send("kunalcom")
+})
+
+app.get("/login", (req, res) => {
+    res.send("<h1>Login</h1>")
+})
+
+app.get("/youtube", (req, res) => {
+    res.send("<h2>chai aur code </h2>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
