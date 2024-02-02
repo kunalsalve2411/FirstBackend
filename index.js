239c6617137a2c09604cb2b64c974f6141ccb98d
kunal. <kunalsalve2411@gmail.com>
2024-02-02 12:31:54 +0530

    another demo route

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get("/youtube", (req, res) => {
     res.send("<h2>chai aur code </h2>")
 })
 
+app.get("/kunalsalve", (req, res) => {
+    res.send("<h1>Euuu Its is next page</h1>")
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
 })
